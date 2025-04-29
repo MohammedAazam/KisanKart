d980816ab8e73eb56e09f47ddefd12f09ef47765
MohammedAazam <mohammedaazam757@gmail.com>
2025-04-29 16:47:03 +0530

    ai price added

--- kisankart/main/views.py
+++ kisankart/main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html')
 
 def ai_price(request):
     return render(request, 'ai_price.html')
+
 
 def products(request):
     return render(request, 'product.html')
