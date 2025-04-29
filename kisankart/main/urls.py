e01f5b193bb177de446e61e8301bf590cfcdcfcd
MohammedAazam <mohammedaazam757@gmail.com>
2025-04-29 16:24:13 +0530

    added profile

--- kisankart/main/urls.py
+++ kisankart/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('ai-price/', views.ai_price, name='ai_price'),
+    path('products/', views.products, name='products'),
+    path('profile/', views.profile, name='profile'),
 ]
--- kisankart/main/views.py
+++ kisankart/main/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def ai_price(request):
+    return render(request, 'ai_price.html')
+
+def products(request):
+    return render(request, 'product.html')
+
+def profile(request):
+    return render(request, 'profile.html')
